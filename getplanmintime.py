f0da470f48303fbf97b3b12d54bac66229ad5e38
Siwakorn Noisundod <jew.siwakorn14@gmail.com>
2023-08-24 15:28:13 +0700

    update api finminhop sematicweb

---
+++ getplanmintime.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify, request, json, Response
+from semantic_Lines_Route import getRoute
+app = Flask(__name__)
+
+
+@app.route("/getplanmintime", methods=['GET'])
+def getplanminhop():
+    try:
+        json_data = request.get_json()
+        output = getRoute(json_data["start_lat"], json_data["start_lon"],
+                          json_data["destination_lat"], json_data["destination_lon"])
+        print(output)
+        return output
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
